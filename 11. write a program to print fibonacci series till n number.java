7e4ca49379172571ff1ea89a7f5528908b882734
Rajpurohit18 <123978275+Rajpurohit18@users.noreply.github.com>
2023-03-12 16:10:35 +0530

    Create 11. write a program to print fibonacci series till n number.java

---
+++ 11. write a program to print fibonacci series till n number.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+        
+
+        int a = 0, b = 1;
+        
+        System.out.print(a+" "+b+" ");
+       for (int i = 1; i <= n; i++) {
+    	   int c= a+b;
+    	   System.out.print(c+" ");
+    	   
+    	   a=b;
+    	   b=c;
+		
+	}
+        
+    }
+}
